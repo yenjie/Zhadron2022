7af144520f5cb04fbca0e448c14e3a84ab72d744
Yen-Jie Lee <yjlee@DESKTOP-21MBO8V.localdomain>
2022-11-08 08:32:20 -0500

    update PDF

--- ZmassAnalysis.C
+++ ZmassAnalysis.C
@@ -53,31 +53,33 @@ Double_t bwfun(Double_t *x, Double_t *par) {
       for(i=1.0; i<=np/2; i++) {
          xx = xlow + (i-.5) * step;
          fland = TMath::BreitWigner(xx,par[0], par[1]);
-//         sum += fland * TMath::Gaus(x[0],xx,par[3]);
-         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
+         sum += fland * TMath::Gaus(x[0],xx,par[3]);
+//         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
          xx = xupp - (i-.5) * step;
          fland = TMath::BreitWigner(xx,par[0], par[1]);
-         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
+         sum += fland * TMath::Gaus(x[0],xx,par[3]);
+//         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
       }
 
-      return (par[2] * step * sum * invsq2pi +par[6]*exp(par[7]*x[0]+par[8]));
-//      return (par[2] * step * sum * invsq2pi / par[3]+par[4]+par[5]*x[0]+par[6]*x[0]*x[0]+par[7]*x[0]*x[0]*x[0]);
+      return (par[2] * step * sum * invsq2pi +par[4]*exp(par[6]+par[5]*x[0]));
+ //     return (par[2] * step * sum * invsq2pi / par[3]+par[4]+par[5]*x[0]+par[6]*x[0]*x[0]+par[7]*x[0]*x[0]*x[0]);
 }
 
 TH1D* ZmassAnalysis(double ptL=0,double ptH=2000,int centL=0,int centH=200)
 {
    TH1D *h = new TH1D("h","",100,0,1);
-   TFile *infData = new TFile("output_doubleMu.root");
+   TFile *infData = new TFile("output_doubleMu_221107.root");
    TFile *infMC = new TFile("outputMC.root");
    
+   TCanvas *c = new TCanvas("c","",800,800);
 
    TTree *tData = (TTree*)infData->Get("t");
    TTree *tMC = (TTree*)infMC->Get("t");
    
-   TH1D *hData = new TH1D("hData","",40,61.2,121.2);
-   TH1D *hDataSame = new TH1D("hDataSame","",40,61.2,121.2);
-   TH1D *hMC = new TH1D("hMC","",40,61.2,121.2);
-   TH1D *hMCSame = new TH1D("hMCSame","",40,61.2,121.2);
+   TH1D *hData = new TH1D("hData","",40,81.2,101.2);
+   TH1D *hDataSame = new TH1D("hDataSame","",40,81.2,101.2);
+   TH1D *hMC = new TH1D("hMC","",40,81.2,101.2);
+   TH1D *hMCSame = new TH1D("hMCSame","",40,81.2,101.2);
    
    tData->Draw("zMass>>hData",Form("zPt>%f&&zPt<%f&&hiBin>=%d&&hiBin<%d",ptL,ptH,centL,centH));
    tData->Draw("zMass>>hDataSame",Form("zPt>%f&&zPt<%f&&hiBin>=%d&&hiBin<%d",ptL,ptH,centL,centH));
@@ -100,46 +102,55 @@ TH1D* ZmassAnalysis(double ptL=0,double ptH=2000,int centL=0,int centH=200)
    hMCSame->SetMarkerColor(2);
 
    
-   hData->Draw("e");
-//   hDataSame->Draw("same");
-   hMC->Draw("e same");
+//   hData->Draw("e");
+//   hMC->Draw("e same");
+ //  hDataSame->Draw("same");
 //   hMCSame->Draw("same");
       
    hData->Scale(1./hData->GetEntries());
    hMC->Scale(1./hMC->GetEntries());
-   hData->Draw();
-   hData->SetXTitle("M_{#mu#mu} (GeV)");
-   hMC->Draw("same"); 
+   hMC->Draw();
+   hMC->SetXTitle("M_{#mu#mu} (GeV)");
+   hData->Draw("same"); 
    hMC->SetLineColor(2);
    hMC->SetMarkerColor(2);
    
 //   TF1 *f = new TF1("f","[0]+[1]*x+[2]*TMath::BreitWigner(x, [3], [4])");
-   TF1 *f = new TF1("f",bwfun,81.2,101.2,9);
+   TF1 *f = new TF1("f",bwfun,81.2,101.2,7);
    f->SetParNames("Mean","Width","Area","GSigma","BkgArea","ExpCont","ExpShift");
-   f->SetParameters(91,3,1.5,1);
+   f->SetParameters(91,4.6,3,0.1,0,0,0,0);
    f->SetLineStyle(2);
    f->SetLineColor(1);
    
  //  TF1 *f2 = new TF1("f2","[0]+[1]*x+[2]*TMath::BreitWigner(x, [3], [4])");
 //   f2->SetParameters(0,0,1,91.2,1);
 
-   TF1 *f2 = new TF1("f2",bwfun,81.2,101.2,9);
+   TF1 *f2 = new TF1("f2",bwfun,81.2,101.2,7);
    f2->SetParNames("Mean","Width","Area","GSigma","BkgArea","ExpCont","ExpShift");
-   f2->SetParameters(91,3,1.5,1);
+   f2->SetParameters(91,4.6,3,0.1,0,0,0,0);
    f2->SetLineStyle(2);
    f2->SetLineColor(2);
    
 
    hData->Fit("f","LL");
    hData->Fit("f","");
+   hData->Fit("f","LL m");
    hData->Fit("f","");
-   hData->Fit("f","LL M");
+   hData->Fit("f","LL m");
+   hData->Fit("f","");
+   hData->Fit("f","");
+   hData->Fit("f","");
+   hData->Fit("f","");
  
+   hMC->Fit("f2","LL m");
+   hMC->Fit("f2","");
    hMC->Fit("f2","LL");
- 
+   hMC->Fit("f2","");
+   hMC->Fit("f2","LL m");
    hMC->Fit("f2","");
    hMC->Fit("f2","");
-   hMC->Fit("f2","LL M");
+   hMC->Fit("f2","");
+   hMC->Fit("f2","");
   
    h->SetBinContent(1,f->GetParameter(0));
    h->SetBinContent(2,f->GetParError(0));
