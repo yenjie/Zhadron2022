08c562b1265cc6372a65e0597ee38580ad630efb
FHead <chen.yi.first@gmail.com>
2023-03-24 15:24:58 +0100

    typo espresso

--- Skims/20230310_ZHadronSkims/PrepareInput.sh
+++ Skims/20230310_ZHadronSkims/PrepareInput.sh
@@ -30,7 +30,7 @@ Condor=Submit.condor
 echo "Universe              = vanilla"           > $Condor
 echo "Executable            = $PWD/RunCondor.sh" >> $Condor
 echo "should_transfer_files = NO"                >> $Condor
-echo "+JobFlavour           = \"expresso\""      >> $Condor
+echo "+JobFlavour           = \"espresso\""      >> $Condor
 echo                                             >> $Condor
 
 Count=0
