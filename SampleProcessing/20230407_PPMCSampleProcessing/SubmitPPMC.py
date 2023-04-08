08f5dffde5dc1abf96a523a81f85e7c2d84bf98a
FHead <chen.yi.first@gmail.com>
2023-04-08 23:53:15 +0200

    new pp MC with gen particle cut

---
+++ SampleProcessing/20230407_PPMCSampleProcessing/SubmitPPMC.py
@@ -0,0 +1,38 @@
+OutputBase = '/store/group/phys_heavyions/chenyi/pp2017/Forest'
+# DatasetName = '/DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbSpring21MiniAOD-FixL1CaloGT_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM'
+# DatasetName = '/HIHardProbes/HIRun2018A-PbPb18_MiniAODv1-v2/MINIAOD'
+# DatasetName = '/SingleMuon/Run2017G-17Nov2017-v1/AOD'
+DatasetName = '/DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV-v2/AODSIM'
+Tag = '20230407_ZHadronMLLWithMuTreePP'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runForestAOD_pp_MC_94X.py'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.maxMemoryMB = 5000
+config.JobType.maxJobRuntimeMin = 4800
+# config.JobType.inputFiles = ['']
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+# config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader/'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName + '/'
+config.Data.ignoreLocality = True
+config.Data.allowNonValidInputDataset = True
+
+config.section_("Site")
+config.Site.blacklist = ['T2_IT_Pisa']
+config.Site.whitelist = ['T2_US_*', 'T2_CH_CERN']
+config.Site.storageSite = 'T2_CH_CERN'
+
