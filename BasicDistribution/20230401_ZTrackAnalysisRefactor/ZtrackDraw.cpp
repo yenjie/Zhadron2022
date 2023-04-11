39da3b8fd88c20e4ae94892b0a0f5be90d60f36a
pinchun <pinchun@mit.edu>
2023-04-11 14:37:05 +0200

    Do not need bwfun here, removed

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackDraw.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackDraw.cpp
@@ -61,65 +61,6 @@ TFile *file_bkgMCgen;
 const char *typeofdata = "20230411";
 const char *typeofdatatext = "double muon";
 
-Double_t bwfun(Double_t *x, Double_t *par) {
-
-   //Fit parameters:
-   
-   //par[0]=mean
-   //par[1]=width
-   //par[2]=area
-   
-   //par[3]=width of the convoluted Gaussian function
-   
-   //par[0]=Width (scale) parameter of Landau density
-   //par[1]=Most Probable (MP, location) parameter of Landau density
-   //par[2]=Total area (integral -inf to inf, normalization constant)
-   //par[3]=Width (sigma) of convoluted Gaussian function
-   //
-   //In the Landau distribution (represented by the CERNLIB approximation),
-   //the maximum is located at x=-0.22278298 with the location parameter=0.
-   //This shift is corrected within this function, so that the actual
-   //maximum is identical to the MP parameter.
-
-      // Numeric constants
-      Double_t invsq2pi = 0.3989422804014;   // (2 pi)^(-1/2)
-      Double_t mpshift  = -0.22278298;       // Landau maximum location
-
-      // Control constants
-      Double_t np = 1000.0;      // number of convolution steps
-      Double_t sc =   5.0;      // convolution extends to +-sc Gaussian sigmas
-
-      // Variables
-      Double_t xx;
-      Double_t mpc;
-      Double_t fland;
-      Double_t sum = 0.0;
-      Double_t xlow,xupp;
-      Double_t step;
-      Double_t i;
-
-      // Range of convolution integral
-      xlow = x[0] - sc * par[3];
-      xupp = x[0] + sc * par[3];
-
-      step = (xupp-xlow) / np;
-
-      // Convolution integral of Landau and Gaussian by sum
-      for(i=1.0; i<=np/2; i++) {
-         xx = xlow + (i-.5) * step;
-         fland = TMath::BreitWigner(xx,par[0], par[1]);
-         sum += fland * TMath::Gaus(x[0],xx,par[3]);
-//         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
-         xx = xupp - (i-.5) * step;
-         fland = TMath::BreitWigner(xx,par[0], par[1]);
-         sum += fland * TMath::Gaus(x[0],xx,par[3]);
-//         sum += fland * ROOT::Math::crystalball_function(x[0], par[3], par[4], 1, xx);
-      }
-
-      return (par[2] * step * sum * invsq2pi +par[4]*exp(par[6]+par[5]*x[0]));
- //     return (par[2] * step * sum * invsq2pi / par[3]+par[4]+par[5]*x[0]+par[6]*x[0]*x[0]+par[7]*x[0]*x[0]*x[0]);
-}
-
 void ZtrackDraw_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
 {
 
