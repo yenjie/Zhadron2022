6506b95acae736087d52e762a8251b85bcd6e106
FHead <chen.yi.first@gmail.com>
2023-04-28 17:29:41 +0200

    high PT track cut

--- CommonCode/source/Messenger.cpp
+++ CommonCode/source/Messenger.cpp
@@ -1611,8 +1611,8 @@ bool PbPbTrackTreeMessenger::PassZHadron2022Cut(int index)
    double ECAL = -1, HCAL = -1;
    if(PFEcal != nullptr && PFEcal->size() > index)   ECAL = PFEcal->at(index);
    if(PFHcal != nullptr && PFHcal->size() > index)   HCAL = PFHcal->at(index);
-   if(TrackPT->at(index) > 20 && ((ECAL + HCAL) / cosh(TrackEta->at(index)) < 0.5 * TrackPT->at(index)))
-      return false;
+   // if(TrackPT->at(index) > 20 && ((ECAL + HCAL) / cosh(TrackEta->at(index)) < 0.5 * TrackPT->at(index)))
+   //    return false;
 
    if(fabs(TrackEta->at(index)) > 2.4)
       return false;
