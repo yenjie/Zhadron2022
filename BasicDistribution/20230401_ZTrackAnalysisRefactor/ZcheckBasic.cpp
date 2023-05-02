a6fcd94d3d26677bae361464f3a4eb2608da7ee0
pinchun <pinchun@mit.edu>
2023-05-02 16:25:04 +0200

    --VZTolerance 10000 and --Tolerance 10 for MCGenBackground

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
@@ -1130,7 +1130,7 @@ int main(int argc, char *argv[]){
    file_ppMC  = TFile::Open("GraphPPMC_0430.root","read");
 
    file_sigMCgen = TFile::Open("GraphMCSignalGen_0430_01.root","read");
-   file_bkgMCgen = TFile::Open("GraphMCBackgroundGen_0502_tf01.root","read");
+   file_bkgMCgen = TFile::Open("GraphMCBackgroundGen_0502.root","read");
 
    file_sigMCgen0Sub = TFile::Open("GraphMCSignalGen0Sub_0430_01.root","read");
 
