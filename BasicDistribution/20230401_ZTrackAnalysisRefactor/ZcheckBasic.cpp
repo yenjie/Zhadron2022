038bbdb95d8dfd6384d6754c4d24f555750fe7c4
pinchun <pinchun@mit.edu>
2023-05-03 00:25:37 +0200

    Date change: 0503

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
@@ -63,7 +63,7 @@ TFile *file_bkgMCgen;
 
 TFile *file_sigMCgen0Sub;
 
-const char *typeofdata = "20230502_tf01";
+const char *typeofdata = "20230503";
 const char *typeofdatatext = "single muon";
 
 void ZcheckBasic_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
@@ -1123,16 +1123,16 @@ int main(int argc, char *argv[]){
 
   style();
 
-   file_sigMC = TFile::Open("GraphMCSignal_0430.root","read");
-   file_bkgMC = TFile::Open("GraphMCBackground_0430.root","read");
-   file_sigDA = TFile::Open("GraphDataSignal_0430.root","read");
-   file_bkgDA = TFile::Open("GraphDataBackground_0430.root","read");
-   file_ppMC  = TFile::Open("GraphPPMC_0430.root","read");
+   file_sigMC = TFile::Open("GraphMCSignal_0503.root","read");
+   file_bkgMC = TFile::Open("GraphMCBackground_0503.root","read");
+   file_sigDA = TFile::Open("GraphDataSignal_0503.root","read");
+   file_bkgDA = TFile::Open("GraphDataBackground_0503.root","read");
+   file_ppMC  = TFile::Open("GraphPPMC_0503.root","read");
 
-   file_sigMCgen = TFile::Open("GraphMCSignalGen_0430_01.root","read");
-   file_bkgMCgen = TFile::Open("GraphMCBackgroundGen_0502.root","read");
+   file_sigMCgen = TFile::Open("GraphMCSignalGen_0503.root","read");
+   file_bkgMCgen = TFile::Open("GraphMCBackgroundGen_0503.root","read");
 
-   file_sigMCgen0Sub = TFile::Open("GraphMCSignalGen0Sub_0430_01.root","read");
+   file_sigMCgen0Sub = TFile::Open("GraphMCSignalGen0Sub_0503.root","read");
 
    
   ZcheckBasic_single(40, 20, 2000,  0, 90,  0, 1000);
