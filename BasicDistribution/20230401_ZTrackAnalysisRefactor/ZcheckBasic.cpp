b762002db543d386138dccec92e18ce73788b1f3
pinchun <pinchun@mit.edu>
2023-05-05 15:34:05 +0200

    Draw 0504.root

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZcheckBasic.cpp
@@ -63,7 +63,7 @@ TFile *file_bkgMCgen;
 
 TFile *file_sigMCgen0Sub;
 
-const char *typeofdata = "20230503";
+const char *typeofdata = "20230505";
 const char *typeofdatatext = "single muon";
 
 void ZcheckBasic_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
@@ -1124,9 +1124,9 @@ int main(int argc, char *argv[]){
   style();
 
    file_sigMC = TFile::Open("GraphMCSignal_0503.root","read");
-   file_bkgMC = TFile::Open("GraphMCBackground_0503.root","read");
+   file_bkgMC = TFile::Open("GraphMCBackground_0504.root","read");
    file_sigDA = TFile::Open("GraphDataSignal_0503.root","read");
-   file_bkgDA = TFile::Open("GraphDataBackground_0503.root","read");
+   file_bkgDA = TFile::Open("GraphDataBackground_0504_01.root","read");
    file_ppMC  = TFile::Open("GraphPPMC_0503.root","read");
 
    file_sigMCgen = TFile::Open("GraphMCSignalGen_0503.root","read");
