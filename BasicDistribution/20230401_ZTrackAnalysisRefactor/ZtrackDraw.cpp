4074c0b301983ad92711dce18660226a5876a36e
pinchun <pinchun@mit.edu>
2023-05-15 15:51:50 +0200

    Date:20230515

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackDraw.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackDraw.cpp
@@ -59,7 +59,7 @@ TFile *file_ppMC;
 TFile *file_sigMCgen;
 TFile *file_bkgMCgen;
 
-const char *typeofdata = "20230513";
+const char *typeofdata = "20230515";
 const char *typeofdatatext = "single muon";
 
 void ZtrackDraw_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
