ff61b7ee929fe46b6d2bed496182d033c727cf16
pinchun <pinchun@mit.edu>
2023-05-18 00:22:00 +0200

    v9_1

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna.sh
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna.sh
@@ -17,40 +17,40 @@ mkdir -p log
 
 if [ $1 -eq 0 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputData_v9/ --Output GraphDataSignal_v9.root --Fraction 1  > log/GraphDataSignal_v9.out 2> log/GraphDataSignal_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputData_v9/ --Output GraphDataSignal_v9_1.root --Fraction 1  > log/GraphDataSignal_v9_1.out 2> log/GraphDataSignal_v9_1.err < /dev/null &
 elif [ $1 -eq 1 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDatabkg_v9/ --Output GraphDataBackground_v9.root --Fraction 1  > log/GraphDataBackground_v9.out 2> log/GraphDataBackground_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDatabkg_v9/ --Output GraphDataBackground_v9_1.root --Fraction 1  > log/GraphDataBackground_v9_1.out 2> log/GraphDataBackground_v9_1.err < /dev/null &
 elif [ $1 -eq 2 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMC_v9/ --Output GraphMCSignal_v9.root --Fraction 1  > log/GraphMCSignal_v9.out 2> log/GraphMCSignal_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMC_v9/ --Output GraphMCSignal_v9_1.root --Fraction 1  > log/GraphMCSignal_v9_1.out 2> log/GraphMCSignal_v9_1.err < /dev/null &
 elif [ $1 -eq 3 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackground_v9/ --Output GraphMCBackground_v9.root --Fraction 1  > log/GraphMCBackground_v9.out 2> log/GraphMCBackground_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackground_v9/ --Output GraphMCBackground_v9_1.root --Fraction 1  > log/GraphMCBackground_v9_1.out 2> log/GraphMCBackground_v9_1.err < /dev/null &
 elif [ $1 -eq 4 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC_v9.root --IgnoreCentrality true --Fraction 1  > log/GraphPPMC_v9.out 2> log/GraphPPMC_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC_v9_1.root --IgnoreCentrality true --Fraction 1  > log/GraphPPMC_v9_1.out 2> log/GraphPPMC_v9_1.err < /dev/null &
 elif [ $1 -eq 5 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC0Sub_v9.root --IgnoreCentrality true --OnlyZeroSub true --Fraction 1  > log/GraphPPMC0Sub_v9.out 2> log/GraphPPMC0Sub_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC0Sub_v9_1.root --IgnoreCentrality true --OnlyZeroSub true --Fraction 1  > log/GraphPPMC0Sub_v9_1.out 2> log/GraphPPMC0Sub_v9_1.err < /dev/null &
 elif [ $1 -eq 6 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen_v9.root --Fraction 1 --DoGenCorrelation true  > log/GraphMCSignalGen_v9.out 2> log/GraphMCSignalGen_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen_v9_1.root --Fraction 1 --DoGenCorrelation true  > log/GraphMCSignalGen_v9_1.out 2> log/GraphMCSignalGen_v9_1.err < /dev/null &
 elif [ $1 -eq 7 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen0Sub_v9.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphMCSignalGen0Sub_v9.out 2> log/GraphMCSignalGen0Sub_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen0Sub_v9_1.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphMCSignalGen0Sub_v9_1.out 2> log/GraphMCSignalGen0Sub_v9_1.err < /dev/null &
 elif [ $1 -eq 8 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen_v9.root --Fraction 1 --DoGenCorrelation true  > log/GraphMCBackgroundGen_v9.out 2> log/GraphMCBackgroundGen_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen_v9_1.root --Fraction 1 --DoGenCorrelation true  > log/GraphMCBackgroundGen_v9_1.out 2> log/GraphMCBackgroundGen_v9_1.err < /dev/null &
 elif [ $1 -eq 9 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen0Sub_v9.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphMCBackgroundGen0Sub_v9.out 2> log/GraphMCBackgroundGen0Sub_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen0Sub_v9_1.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphMCBackgroundGen0Sub_v9_1.out 2> log/GraphMCBackgroundGen0Sub_v9_1.err < /dev/null &
 elif [ $1 -eq 10 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen_v9.root --IgnoreCentrality true --Fraction 1 --DoGenCorrelation true  > log/GraphPPMCGen_v9.out 2> log/GraphPPMCGen_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen_v9_1.root --IgnoreCentrality true --Fraction 1 --DoGenCorrelation true  > log/GraphPPMCGen_v9_1.out 2> log/GraphPPMCGen_v9_1.err < /dev/null &
 elif [ $1 -eq 11 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen0Sub_v9.root --IgnoreCentrality true  --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphPPMCGen_v9.out 2> log/GraphPPMCGen_v9.err < /dev/null &
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen0Sub_v9_1.root --IgnoreCentrality true  --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true  > log/GraphPPMCGen_v9_1.out 2> log/GraphPPMCGen_v9_1.err < /dev/null &
 else
 	echo "Input number exceeded"
 fi
