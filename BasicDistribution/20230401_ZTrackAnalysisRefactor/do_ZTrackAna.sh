0db3f859d2efcddc536fb17ef31c15b397e617d2
pinchun <pinchun@mit.edu>
2023-05-24 18:49:30 +0200

    Ana v10

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna.sh
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna.sh
@@ -15,40 +15,40 @@ mkdir -p log
 
 if [ $1 -eq 0 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputData_v9/ --Output GraphDataSignal_v9_1.root --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputData_v10/ --Output GraphDataSignal_v10.root --Fraction 1
 elif [ $1 -eq 1 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDatabkg_v9/ --Output GraphDataBackground_v9_1.root --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDatabkg_v10/ --Output GraphDataBackground_v10.root --Fraction 1
 elif [ $1 -eq 2 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMC_v9/ --Output GraphMCSignal_v9_1.root --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMC_v10/ --Output GraphMCSignal_v10.root --Fraction 1
 elif [ $1 -eq 3 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackground_v9/ --Output GraphMCBackground_v9_1.root --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackground_v10/ --Output GraphMCBackground_v10.root --Fraction 1
 elif [ $1 -eq 4 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC_v9_1.root --IgnoreCentrality true --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v10/ --Output GraphPPMC_v10.root --IgnoreCentrality true --Fraction 1
 elif [ $1 -eq 5 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v9/ --Output GraphPPMC0Sub_v9_1.root --IgnoreCentrality true --OnlyZeroSub true --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMC_v10/ --Output GraphPPMC0Sub_v10.root --IgnoreCentrality true --OnlyZeroSub true --Fraction 1
 elif [ $1 -eq 6 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen_v9_1.root --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v10/ --Output GraphMCSignalGen_v10.root --Fraction 1 --DoGenCorrelation true
 elif [ $1 -eq 7 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v9/ --Output GraphMCSignalGen0Sub_v9_1.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGen_v10/ --Output GraphMCSignalGen0Sub_v10.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
 elif [ $1 -eq 8 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen_v9_1.root --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v10/ --Output GraphMCBackgroundGen_v10.root --Fraction 1 --DoGenCorrelation true
 elif [ $1 -eq 9 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v9/ --Output GraphMCBackgroundGen0Sub_v9_1.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCbkgGen_v10/ --Output GraphMCBackgroundGen0Sub_v10.root --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
 elif [ $1 -eq 10 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen_v9_1.root --IgnoreCentrality true --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v10/ --Output GraphPPMCGen_v10.root --IgnoreCentrality true --Fraction 1 --DoGenCorrelation true
 elif [ $1 -eq 11 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v9/ --Output GraphPPMCGen0Sub_v9_1.root --IgnoreCentrality true  --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCGen_v10/ --Output GraphPPMCGen0Sub_v10.root --IgnoreCentrality true  --OnlyZeroSub true --Fraction 1 --DoGenCorrelation true
 else
 	echo "Input number exceeded"
 fi
