053becaac0b9660e20e6f2f2c3c4cb8f5987a00e
pinchun <pinchun@mit.edu>
2023-11-29 14:07:09 +0100

    ok

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna1.sh
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna1.sh
@@ -17,16 +17,16 @@ mkdir -p log
 
 if [ $1 -eq 0 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEUp100_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEUp100_v17_PFmuon.root --Fraction 1
-elif [ $1 -eq 1 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEDown100_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEDown100_v17_PFmuon.root --Fraction 1
-elif [ $1 -eq 2 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEUp200_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEUp200_v17_PFmuon.root --Fraction 1
-elif [ $1 -eq 3 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEDown200_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEDown200_v17_PFmuon.root --Fraction 1
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDataSigBkg_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphDataSigBkg_v17_PFmuon.root --Fraction 1
+#elif [ $1 -eq 1 ]
+#then
+#	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEDown100_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEDown100_v17_PFmuon.root --Fraction 1
+#elif [ $1 -eq 2 ]
+#then
+#	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEUp200_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEUp200_v17_PFmuon.root --Fraction 1
+#elif [ $1 -eq 3 ]
+#then
+#	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCBackgroundUEDown200_v17_PFmuon/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCBackgroundUEDown200_v17_PFmuon.root --Fraction 1
 else
 	echo "Input number exceeded"
 fi
