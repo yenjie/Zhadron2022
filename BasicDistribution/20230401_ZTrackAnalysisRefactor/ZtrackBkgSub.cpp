87abb193cbcdbf1628f87ca14c22718c83f5ecca
pinchun <pinchun@mit.edu>
2023-12-01 16:18:33 +0100

    BkgSub: SigBkg data

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackBkgSub.cpp
@@ -74,7 +74,7 @@ TFile *file_sigMCgen;
 TFile *file_bkgMCgen;
 
 const char *typeofdata = "v17_PFMuon/20231201";
-const char *typeofdata1 = "v17_PF_20231201_SigBkg";
+const char *typeofdata1 = "v17_PF_20231201_SigBkgData";
 const char *typeofdatatext = "single muon";
 
 void ZtrackBkg_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
@@ -323,7 +323,7 @@ void ZtrackBkg_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,fl
 
    if(TptL==0) TptL=TptL_min;
 
-   TLatex *pt0 = new TLatex(0.15,0.82,"Z - Z #times Z (MC)");
+   TLatex *pt0 = new TLatex(0.15,0.82,"Z - Z #times Z (Data)");
    pt0->SetTextFont(42);
    pt0->SetTextSize(0.03);
    pt0->SetNDC(kTRUE);
@@ -531,12 +531,13 @@ int main(int argc, char *argv[]){
 
    style();
 
-   file_sigMC = TFile::Open("~/eos_base/BasicPlots/GraphMCSignal_v17_PFmuon.root","read");
-   file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCSigBkg_v17_PFmuon.root","read");
+   file_sigMC = TFile::Open("~/eos_base/BasicPlots/GraphDataSignal_v17_PFmuon.root","read");
+   file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphDataSigBkg_v17_PFmuon.root","read");
    //file_sigDA = TFile::Open("~/eos_base/BasicPlots/GraphDataSignal_v17_PFmuon.root","read");
    //file_bkgDA = TFile::Open("~/eos_base/BasicPlots/GraphDataBackground_v17_PFmuon.root","read");
-   file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMC0Sub_v17_PFmuon.root","read");
+   //file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMC0Sub_v17_PFmuon.root","read");
    //file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMC_v17_PFmuon.root","read");
+   file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPData_v17_PFmuon.root","read");
 
    file_sigMCgen = TFile::Open("~/eos_base/BasicPlots/GraphMCSignalGen_v17_PFmuon.root","read");
    //file_sigMCgen = TFile::Open("~/eos_base/BasicPlots/GraphMCSignalGen0Sub_v17_PFmuon.root","read");
