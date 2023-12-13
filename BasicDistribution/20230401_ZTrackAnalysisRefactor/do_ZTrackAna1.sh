2ec0c751bba2ba565a0f72eb32f7e634b1fb186a
pinchun <pinchun@mit.edu>
2023-12-13 21:33:24 +0100

    MaximumCentrality Cent10 ZTrackAna

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna1.sh
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/do_ZTrackAna1.sh
@@ -15,21 +15,12 @@ cd /afs/cern.ch/user/p/pchou/PhysicsHIZHadron2022/BasicDistribution/20230401_ZTr
 
 mkdir -p log
 
-if [ $1 -eq 4 ]
+if [ $1 -eq 0 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPDataSigBkg_v17_checkz/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphPPDataSigBkg_v17_checkz.root --Fraction 1 --IgnoreCentrality true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCSigBkg_v17_Cent10/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCSigBkg_v17_Cent10.root --Fraction 1
 elif [ $1 -eq 1 ]
 then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCSigBkg_v17_checkz/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphPPMCSigBkg_v17_checkz.root --Fraction 1 --IgnoreCentrality true
-elif [ $1 -eq 2 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputDataSigBkg_v17_checkz/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphDataSigBkg_v17_checkz.root --Fraction 1
-elif [ $1 -eq 3 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCSigBkg_v17_checkz/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCSigBkg_v17_checkz.root --Fraction 1
-elif [ $1 -eq 0 ]
-then
-	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputMCGenSigBkg_v17_checkz/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphMCGenSigBkg_v17_checkz.root --Fraction 1  --DoGenCorrelation true
+	./Execute --InputBase /eos/cms/store/group/phys_heavyions/pchou/OutputPPMCSigBkg_v17_Cent10/ --Output /eos/cms/store/group/phys_heavyions/pchou/BasicPlots/GraphPPMCSigBkg_v17_Cent10.root --Fraction 1 --IgnoreCentrality true
 else
 	echo "Input number exceeded"
 fi
