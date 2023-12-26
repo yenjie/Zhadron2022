1deae66d1ed69183a52f8cc805f675ad6d0f672c
pinchun <pinchun@mit.edu>
2023-12-26 18:25:27 +0100

    BkgSub SigBkg 300 1

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZtrackBkgSub.cpp
@@ -74,8 +74,8 @@ TFile *file_ppbkgMC;
 TFile *file_sigMCgen;
 TFile *file_bkgMCgen;
 
-const char *typeofdata = "v17d_PFMuon/20231225";
-const char *typeofdata1 = "sigbkg_01";
+const char *typeofdata = "v17d_PFMuon/20231226";
+const char *typeofdata1 = "sigbkg";
 
 void ZtrackBkg_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000)
 {
@@ -549,7 +549,7 @@ int main(int argc, char *argv[]){
 
    file_sigMC = TFile::Open("~/eos_base/BasicPlots/GraphMCSignal_v17_PFmuon.root","read");
    //file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v17d_PFmuon.root","read");
-   file_bkgMC = TFile::Open("GraphMCSigBkg_v17d_PFmuon_300_01.root","read");
+   file_bkgMC = TFile::Open("GraphMCSigBkg_v17d_PFmuon_300.root","read");
    //file_sigDA = TFile::Open("~/eos_base/BasicPlots/GraphDataSignal_v17_PFmuon.root","read");
    //file_bkgDA = TFile::Open("~/eos_base/BasicPlots/GraphDataBackground_v17_PFmuon.root","read");
    //file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMC0Sub_v17_PFmuon.root","read");
