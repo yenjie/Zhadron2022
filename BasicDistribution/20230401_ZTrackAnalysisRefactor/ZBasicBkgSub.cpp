1e05f9cf4e0978322e07b7d0f3b3026b0f2e9eba
pinchun <pinchun@mit.edu>
2024-01-02 23:59:48 +0100

    ZBasicBkgSub nominal 37 ov10 0.1

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
@@ -74,8 +74,8 @@ TFile *file_ppMC;
 TFile *file_ppbkgMC;
 
 
-const char *typeofdata = "v17d_PFMuon/20240102/nominal_ov1";
-const char *typeofdata1 = "37_ov1";
+const char *typeofdata = "v17d_PFMuon/20240102/nominal_ov10_01";
+const char *typeofdata1 = "37_ov10_01";
 
 //const char *typeofdata = "v17d_PFMuon/20240102/SigBkg";
 //const char *typeofdata1 = "350_ov5";
@@ -427,7 +427,7 @@ int main(int argc, char *argv[]){
    if(selfmix)
       file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCSigBkg_v17d_PFmuon_350_ov5.root","read");
    else
-      file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v17d_PFmuon_ov1.root","read");
+      file_bkgMC = TFile::Open("~/eos_base/BasicPlots/GraphMCBackground_v17d_PFmuon_01.root","read");
    
    file_ppMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMC0Sub_v17_PFmuon.root","read");
    file_ppbkgMC  = TFile::Open("~/eos_base/BasicPlots/GraphPPMCSigBkg_v17d_Cent10_143.root","read");
