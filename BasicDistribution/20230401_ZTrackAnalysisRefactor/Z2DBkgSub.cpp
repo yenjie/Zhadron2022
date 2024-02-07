6582e5b8301a806c03304a19404e5ae0086e5956
pinchun <pinchun@mit.edu>
2024-02-07 00:07:48 -0500

    Z2DBkgSub SigBkg

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/Z2DBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/Z2DBkgSub.cpp
@@ -74,15 +74,13 @@ TFile *file_ppMC;
 TFile *file_ppbkgMC;
 
 
-const char *typeofdata = "v17d_PFMuon/20240206/nominal_ov10_10HF_RECO";
-//const char *typeofdata = "testBkgSub/20240204/v17d_No1Sub";
-//const char *typeofdata1 = "no1sub";
-const char *typeofdata1 = "37_ov10_RECO";
+//const char *typeofdata = "v17d_PFMuon/20240206/nominal_ov10_10HF_RECO";
+//const char *typeofdata1 = "37_ov10_RECO";
 
-//const char *typeofdata = "v17d_PFMuon/20240204/SigBkg_ov20_10HF";
-//const char *typeofdata1 = "350_ov20_pp10HF";
+const char *typeofdata = "v17d_PFMuon/20240206/SigBkg_ov20_10HF";
+const char *typeofdata1 = "350_ov20_pp10HF";
 
-bool selfmix = false;
+bool selfmix = true;
 
 void Z2DBkgSub_single(int binnum=40,float ptL=20,float ptH=2000,float centL=0,float centH=90,float TptL=0,float TptH=10000, 
    string HistName="HPhi", string XTitleName = "#Delta#phi_{Z,track}", string YTitleName = "dN/d#Delta#phi", string ZTitleName = "dN/d#Delta#eta#Delta#phi", int rebinnum=1)
