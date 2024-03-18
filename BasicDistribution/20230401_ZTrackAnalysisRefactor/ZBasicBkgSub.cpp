8915160656c59d55613e33d3974c5c0dd754da2e
pinchun <pinchun@mit.edu>
2024-03-18 12:17:39 -0400

    BasicBkgSub MCGenBackground mZ

--- BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
+++ BasicDistribution/20230401_ZTrackAnalysisRefactor/ZBasicBkgSub.cpp
@@ -74,7 +74,7 @@ TFile *file_ppMC;
 TFile *file_ppbkgMC;
 
 
-const char *typeofdata = "v18/20240317/Gen_HydjetV18_8167";
+const char *typeofdata = "v18/20240318/Gen_HydjetV18_mZ";
 //const char *typeofdata = "testBkgSub/20240204/v17d_No1Sub";
 //const char *typeofdata1 = "no1sub";
 //const char *typeofdata1 = "37_ov10_RECO_PP";
